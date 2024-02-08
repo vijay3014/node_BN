e558a34b29be1c1ba09e4ffab116010af889a4ef
vijay solanki <vijaysolanki1481@gmail.com>
2024-02-08 09:17:59 +0530

    8-feb

--- typescript/first.ts
+++ typescript/first.ts
@@ -1,2 +1,2 @@
-let a=10
-console.log(a)
+// let a=10
+// console.log(a)
---
+++ typescript/function.ts
@@ -0,0 +1,30 @@
+// function Add (a:number,b:number):number
+// {
+//     return a+b
+// }
+
+// console.log(Add(2,5))
+// console.log(Add(2,'5'))
+
+// interface hello{
+//     name ?:string
+//     email ?:string
+//     age ?: number
+// }
+
+// let person : hello ={
+//     name:'john',
+//     age :30
+//     // job:'Developer'
+// }
+
+// console.log(person);
+
+// enum gender{
+//     'male','female','hello'
+// }
+
+// let x = gender.male
+
+// console.log(x)
+
